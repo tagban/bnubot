3362bab4e1f7b467f4e5ffea6222bd112026fec1
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-05-25 06:00:50 +0000

    *** empty log message ***

--- trunk/BNUBot/src/Main.java
+++ trunk/BNUBot/src/Main.java
@@ -1,23 +1,28 @@
 
 import core.*;
 import core.bncs.BNCSConnection;
+import core.bnftp.BNFTPConnection;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
 		ConnectionSettings cs = new ConnectionSettings();
 		cs.autoconnect = true;
-		cs.cdkey = "2148022963770";
+		//cs.cdkey = "2148022963770";
+		//cs.cdkey = "C489Y449XM66MDHNYZKGGE6PHP";
+		cs.cdkey = "2TK68RK6CKPBV2C2";
 		cs.password = "test";
 		cs.username = "test";
 		if(true) {
-			//cs.server = "uswest.battle.net";
-			cs.server = "pyroserver.no-ip.org";
+			cs.server = "uswest.battle.net";
 			cs.port = 6112;
 		} else {
 			cs.server = "sigpi.ath.cx";
 			cs.port = 80;
 		}
+		
+		//BNFTPConnection f = new BNFTPConnection(cs, "Icons.bni");
+		//f.run();
 		
 		EventHandler e = null;
 		BNCSConnection c = new BNCSConnection(cs, e);
--- trunk/BNUBot/src/core/Connection.java
+++ trunk/BNUBot/src/core/Connection.java
@@ -2,11 +2,9 @@ package core;
 
 public abstract class Connection extends Thread {
 	protected ConnectionSettings cs;
-	protected EventHandler e;
 	
-	public Connection(ConnectionSettings cs, EventHandler e) {
+	public Connection(ConnectionSettings cs) {
 		this.cs = cs;
-		this.e = e;
 	}
 
 	public abstract void Connect();
