9a17fe66cec9328abd006d4877524b8c90e6626f
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-05-26 00:06:00 +0000

    *** empty log message ***

--- BNUBot/src/Main.java
+++ BNUBot/src/Main.java
@@ -7,25 +7,7 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		ConnectionSettings cs = new ConnectionSettings();
-		cs.autoconnect = true;
-		
-		cs.cdkey = "2148022963770";
-		cs.product = ConnectionSettings.PRODUCT_STARCRAFT;
-		
-		cs.cdkey = "C489Y449XM66MDHNYZKGGE6PHP";
-		cs.product = ConnectionSettings.PRODUCT_WARCRAFT3;
-		
-		//cs.cdkey = "2TK68RK6CKPBV2C2";
-		//cs.product = ConnectionSettings.PRODUCT_DIABLO2;
-		
-		cs.username = "BNU-Bot";
-		cs.password = "012040a";
-		
-		cs.server = "useast.battle.net";
-		cs.port = 6112;
-		
-		//cs.server = "sigpi.ath.cx";
-		//cs.port = 80;
+		cs.load();
 		
 		//BNFTPConnection f = new BNFTPConnection(cs, "Icons.bni");
 		//f.start();
