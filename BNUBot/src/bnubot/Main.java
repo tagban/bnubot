fa63b4bc37653807173510dd78545fe2496f60e9
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-05-30 21:52:23 +0000

    Added command-line arguments

--- BNUBot/src/bnubot/Main.java
+++ BNUBot/src/bnubot/Main.java
@@ -14,6 +14,35 @@ public class Main {
 		ConnectionSettings cs = new ConnectionSettings();
 		cs.load();
 		
+		for(int i = 0; i < args.length; i++) {
+			if(args[i].charAt(0) == '-') {
+				switch(args[i].charAt(1)) {
+				case 'c':
+					if(args[i].compareToIgnoreCase("-cli") == 0) {
+						cs.enableCLI = true;
+						continue;
+					}
+				case 'g':
+					if(args[i].compareToIgnoreCase("-gui") == 0) {
+						cs.enableGUI = true;
+						continue;
+					}
+				case 'n':
+					if(args[i].compareToIgnoreCase("-nocli") == 0) {
+						cs.enableCLI = false;
+						continue;
+					}
+					if(args[i].compareToIgnoreCase("-nogui") == 0) {
+						cs.enableGUI = false;
+						continue;
+					}
+				}
+			}
+			
+			System.err.println("Invalid argument: " + args[i]);
+			System.exit(1);
+		}
+		
 		if(!cs.isValid()) {
 			ConfigurationFrame cf = new ConfigurationFrame(cs);
 			cf.setVisible(true);
