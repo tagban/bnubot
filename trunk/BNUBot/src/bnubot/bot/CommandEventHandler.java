dd8627ce8597dfd7d75e267cae9e110fd772e3f9
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-01 16:15:37 +0000

    Whisper commands

--- trunk/BNUBot/src/bnubot/bot/CommandEventHandler.java
+++ trunk/BNUBot/src/bnubot/bot/CommandEventHandler.java
@@ -18,6 +18,117 @@ public class CommandEventHandler implements EventHandler {
 	public void initialize(Connection c) {
 		this.c = c;
 	}
+	
+	private void parseCommand(String user, String command, String param) {
+		String[] params = param.split(" ");
+		
+		switch(command.charAt(0)) {
+		case 'a':
+			if(command.compareToIgnoreCase("add") == 0) {
+				try {
+					if(params.length != 2)
+						throw new Exception();
+					int access = Integer.valueOf(params[1]);
+					d.getUserDatabase().addUser(params[0], new User(access));
+					
+					c.sendChat("Added user [" + params[0] + "] sucessfully with access " + access);
+				} catch(Exception e) {
+					c.sendChat("Usage: ~add <user>[@<realm>] <access>");
+					break;
+				}
+				break;
+			}
+			
+		case 'b':
+			if(command.compareToIgnoreCase("ban") == 0) {
+				if(param.length() == 0) {
+					c.sendChat("Usage: ~ban <user>[@<realm>] [reason]");
+					break;
+				}
+				c.sendChat("/ban " + param);
+				break;
+			}
+		
+		case 'i':
+			if(command.compareToIgnoreCase("info") == 0) {
+				Properties p = System.getProperties();
+				c.sendChat("BNU-Bot v2.0 beta running on " + p.getProperty("os.name") + " (" + p.getProperty("os.arch") + ")");
+				break;
+			}
+			
+		case 'k':
+			if(command.compareToIgnoreCase("kick") == 0) {
+				if(params.length != 1) {
+					c.sendChat("Usage: ~kick <user>[@<realm>]");
+					break;
+				}
+				c.sendChat("/kick " + params[0]);
+				break;
+			}
+		case 's':
+			if(command.compareToIgnoreCase("setrank") == 0) {
+				int newRank;
+				try {
+					if(params.length != 2)
+						throw new Exception();
+					newRank = Integer.valueOf(params[1]);
+					if((newRank < 1) || (newRank > 3))
+						throw new Exception();
+				} catch(Exception e) {
+					c.sendChat("Usage: ~setrank <user> <rank:1-3>");
+					break;
+				}
+				
+				// TODO: validate that params[0] is in the clan
+				try {
+					c.setClanRank(params[0], newRank);
+					c.sendChat("Success");
+				} catch(Exception e) {
+					c.sendChat("Failure");
+				}
+				break;
+			}
+			if(command.compareToIgnoreCase("sweepban") == 0) {
+				if(params.length < 1) {
+					c.sendChat("Usage: ~sweepban <channel>");
+					break;
+				}
+				sweepBanInProgress = true;
+				sweepBannedUsers = 0;
+				c.sendChat("/who " + param);
+				break;
+			}
+		case 'u':
+			if(command.compareToIgnoreCase("unban") == 0) {
+				if(params.length != 1) {
+					c.sendChat("Usage: ~unban <user>[@<realm>]");
+					break;
+				}
+				c.sendChat("/unban " + params[0]);
+				break;
+			}
+		case 'w':
+			if(command.compareToIgnoreCase("whois") == 0) {
+				if(params.length != 1) {
+					c.sendChat("Usage: ~whois <user>[@realm]");
+					break;
+				}
+				
+				User usr = d.getUserDatabase().getUser(params[0]);
+				
+				if(usr == null) {
+					c.sendChat("User [" + params[0] + "] not found in database");
+					break;
+				}
+				
+				c.sendChat("User [" + params[0] + "] has access " + usr.getAccess());
+				break;
+			}
+		default:
+			c.sendChat("Command not recognized.");
+		}
+	}
+	
 
 	public void channelJoin(String user, int flags, int ping, String statstr) {
 		if((user.compareToIgnoreCase("bnu-camel@azeroth") == 0)
@@ -27,15 +138,9 @@ public class CommandEventHandler implements EventHandler {
 		}
 	}
 
-	public void channelLeave(String user, int flags, int ping, String statstr) {
-	}
-
-	public void channelUser(String user, int flags, int ping, String statstr) {
-	}
-
-	public void joinedChannel(String channel) {
-		c.sendChat("/me is a BNU-Bot");
-	}
+	public void channelLeave(String user, int flags, int ping, String statstr) {}
+	public void channelUser(String user, int flags, int ping, String statstr) {}
+	public void joinedChannel(String channel) {}
 
 	public void recieveChat(String user, String text) {
 		User u = d.getUserDatabase().getUser(user);
@@ -48,126 +153,21 @@ public class CommandEventHandler implements EventHandler {
 		if(text.length() == 0)
 			return;
 		
-		if(text.charAt(0) == '~') {
+		char trigger = c.getConnectionSettings().trigger.charAt(0);
+		
+		if(text.compareToIgnoreCase("?trigger") == 0)
+			c.sendChat("The bot's trigger is: " + trigger);
+		
+		if(text.charAt(0) == trigger) {
 			String[] command = text.substring(1).split(" ");
 			String paramString = text.substring(text.indexOf(' ') + 1);
 		
-			switch(command[0].charAt(0)) {
-			case 'a':
-				if(command[0].compareToIgnoreCase("add") == 0) {
-					try {
-						if(command.length != 3)
-							throw new Exception();
-						int access = Integer.valueOf(command[2]);
-						d.getUserDatabase().addUser(command[1], new User(access));
-						
-						c.sendChat("Added user [" + command[1] + "] sucessfully with access " + access);
-					} catch(Exception e) {
-						c.sendChat("Usage: ~add <user>[@<realm>] <access>");
-						break;
-					}
-					break;
-				}
-				
-			case 'b':
-				if(command[0].compareToIgnoreCase("ban") == 0) {
-					if(command.length == 1) {
-						c.sendChat("Usage: ~ban <user>[@<realm>] [reason]");
-						break;
-					}
-					c.sendChat("/" + text.substring(1));
-					break;
-				}
-			
-			case 'i':
-				if(command[0].compareToIgnoreCase("info") == 0) {
-					Properties p = System.getProperties();
-					c.sendChat("BNU-Bot v2.0 beta running on " + p.getProperty("os.name") + " (" + p.getProperty("os.arch") + ")");
-					break;
-				}
-				
-			case 'k':
-				if(command[0].compareToIgnoreCase("kick") == 0) {
-					if(command.length <= 1) {
-						c.sendChat("Usage: ~kick <user>[@<realm>] [reason]");
-						break;
-					}
-					break;
-				}
-			case 's':
-				if(command[0].compareToIgnoreCase("setrank") == 0) {
-					int newRank;
-					try {
-						if(command.length != 3)
-							throw new Exception();
-						newRank = Integer.valueOf(command[2]);
-						if((newRank < 1) || (newRank > 3))
-							throw new Exception();
-					} catch(Exception e) {
-						c.sendChat("Usage: ~setrank <user> <rank:1-3>");
-						break;
-					}
-					
-					// TODO: validate that command[1] is in the clan
-					try {
-						c.setClanRank(command[1], newRank);
-						c.sendChat("Success");
-					} catch(Exception e) {
-						c.sendChat("Failure");
-					}
-					break;
-				}
-				if(command[0].compareToIgnoreCase("sweepban") == 0) {
-					if(command.length < 2) {
-						c.sendChat("Usage: ~sweepban <channel>");
-						break;
-					}
-					sweepBanInProgress = true;
-					sweepBannedUsers = 0;
-					c.sendChat("/who " + paramString);
-					break;
-				}
-			case 'u':
-				if(command[0].compareToIgnoreCase("unban") == 0) {
-					if(command.length != 2) {
-						c.sendChat("Usage: ~unban <user>[@<realm>]");
-						break;
-					}
-					c.sendChat("/unban " + command[1]);
-					break;
-				}
-			case 'w':
-				if(command[0].compareToIgnoreCase("whois") == 0) {
-					if(command.length != 2) {
-						c.sendChat("Usage: ~whois <user>[@realm]");
-						break;
-					}
-					
-					User usr = d.getUserDatabase().getUser(command[1]);
-					
-					if(usr == null) {
-						c.sendChat("User [" + command[1] + "] not found in database");
-						break;
-					}
-					
-					c.sendChat("User [" + command[1] + "] has access " + u.getAccess());
-					break;
-				}
-			default:
-				c.sendChat("Command not recognized.");
-			}
+			parseCommand(user, command[0], paramString);
 		}
 	}
 
-	public void recieveEmote(String user, String text) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void recieveError(String text) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void recieveEmote(String user, String text) {}
+	public void recieveError(String text) {}
 
 	// If the name is "[NAME]", return "NAME" otherwise pass name through
 	private String removeOpUserBrackets(String name) {
@@ -178,8 +178,8 @@ public class CommandEventHandler implements EventHandler {
 		}
 		return name;
 	}
+	
 	public void recieveInfo(String text) {
-		// TODO Auto-generated method stub
 		if(sweepBanInProgress) {
 			boolean turnItOff = true;
 			
@@ -216,24 +216,26 @@ public class CommandEventHandler implements EventHandler {
 		
 	}
 
-	public void bnetConnected() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void bnetDisconnected() {
-		// TODO Auto-generated method stub
-		
-	}
+	public void bnetConnected() {}
+	public void bnetDisconnected() {}
 
 	public void whisperRecieved(String user, String text) {
-		// TODO Auto-generated method stub
+		User u = d.getUserDatabase().getUser(user);
+		if(u == null)
+			return;
+		if(u.getAccess() <= 0)
+			return;
+		if(text == null)
+			return;
+		if(text.length() == 0)
+			return;
 		
+		String[] command = text.substring(1).split(" ");
+		String paramString = text.substring(text.indexOf(' ') + 1);
+		
+		parseCommand(user, command[0], paramString);
 	}
 
-	public void whisperSent(String user, String text) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void whisperSent(String user, String text) {}
 
 }
