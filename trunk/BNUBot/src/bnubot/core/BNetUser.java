e12bdb167da18a699b5b8b4e20b2dde009c30428
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-04 19:50:22 +0000

    More documentation

--- trunk/BNUBot/src/bnubot/core/BNetUser.java
+++ trunk/BNUBot/src/bnubot/core/BNetUser.java
@@ -1,10 +1,15 @@
 package bnubot.core;
 
 public class BNetUser {
-	String shortLogonName;	// #=yes, realm=only if necessary
+	String shortLogonName;	// #=yes, realm=only if different from "myRealm"
 	String fullLogonName;	// #=yes, realm=yes
 	String fullAccountName;	// #=no, realm=yes
 
+	/**
+	 * Constructor for a BNetUser
+	 * @param user		User[@Realm][#N]
+	 * @param myRealm	[User@]Realm
+	 */
 	public BNetUser(String user, String myRealm) {
 		String uAccount;
 		String uRealm;
@@ -13,6 +18,9 @@ public class BNetUser {
 		int i = myRealm.indexOf('@');
 		if(i != -1)
 			myRealm = myRealm.substring(i + 1);
+		i = myRealm.indexOf('#');
+		if(i != -1)
+			myRealm = myRealm.substring(0, i);
 		
 		i = user.indexOf('#');
 		if(i != -1) {
@@ -45,23 +53,39 @@ public class BNetUser {
 		fullAccountName = uAccount + "@" + uRealm;
 	}
 	
+	/**
+	 * Gets the shortest possible logon name
+	 * @return User[@Realm][#N]
+	 */
 	public String getShortLogonName() {
 		return shortLogonName;
 	}
 	
+	/**
+	 * Gets the full logon name
+	 * @return User@Realm[#N]
+	 */
 	public String getFullLogonName() {
 		return fullLogonName;
 	}
 	
+	/**
+	 * Gets the full account name
+	 * @return "User@Realm"
+	 */
 	public String getFullAccountName() {
 		return fullAccountName;
 	}
 	
+	/**
+	 * Equivalent to getShortLogonName
+	 * @return User[@Realm][#N]
+	 */
 	public String toString() {
 		return shortLogonName;
 	}
 	
-	public boolean Equals(Object o) {
+	public boolean equals(Object o) {
 		if(o instanceof BNetUser) {
 			BNetUser u = (BNetUser)o;
 			if(u.getFullLogonName().equals(fullLogonName))
