cf726aba0e7b6da971ea18f70b35460c8b311d75
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-05 21:11:07 +0000

    Don't allow /kick /ban unless the bot is an op

--- trunk/BNUBot/src/bnubot/core/queue/ChatQueue.java
+++ trunk/BNUBot/src/bnubot/core/queue/ChatQueue.java
@@ -10,15 +10,15 @@ public class ChatQueue extends Thread {
 	LinkedList<String> queue = new LinkedList<String>();
 	
 	public ChatQueue() {
-		setDaemon(true);
+		this.setDaemon(true);
 	}
 	
 	public boolean add(Connection c) {
 		return cons.add(c);
 	}
 	
-	public void enqueue(String text) {
-		queue.add(text);
+	public boolean enqueue(String text) {
+		return queue.add(text);
 	}
 	
 	public void run() {
@@ -31,8 +31,35 @@ public class ChatQueue extends Thread {
 					Connection con = it.next();
 					// Check if the con can send text now
 					if(con.canSendChat()) {
-						// Write the text out
-						con.sendChatNow(queue.remove());
+						if(!con.isOp()) {
+							//Find a string we can send
+							int queueIndex;
+							findSendableText: for(queueIndex = 0; queueIndex < queue.size(); queueIndex++) {
+								String text = queue.get(queueIndex);
+								
+								// Check if ops is required
+								try {
+									if(text.charAt(0) == '/') {
+										String cmd = text.substring(1);
+										int i = cmd.indexOf(' ');
+										if(i != -1) {
+											cmd = cmd.substring(0, i).toLowerCase();
+											
+											if(cmd.equals("kick")
+											|| cmd.equals("ban"))
+												continue findSendableText;
+										}
+									}
+								} catch(Exception e) {}
+
+								// Write the text out
+								con.sendChatNow(queue.remove(queueIndex));
+							}
+						} else {
+							// Write the text out
+							con.sendChatNow(queue.remove());
+						}
+						
 						// If the queue is empty, stop
 						if(queue.size() == 0)
 							break;
