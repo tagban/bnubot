4b44249f49f37975ece18f93682176fdbdcfdd7a
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-06 20:03:58 +0000

    *** empty log message ***

---
+++ BNUBot/src/bnubot/core/bncs/PlatformIDs.java
@@ -0,0 +1,7 @@
+package bnubot.core.bncs;
+
+public interface PlatformIDs {
+		public static final int PLATFORM_IX86	= 0x49583836;
+		public static final int PLATFORM_PMAC	= 0x504D4143;
+		public static final int PLATFORM_XMAC	= 0x584D4143;
+}
