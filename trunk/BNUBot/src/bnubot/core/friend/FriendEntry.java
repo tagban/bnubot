f1285707578a28214ff62a0e1416fae8478823c2
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-06 17:13:15 +0000

    *** empty log message ***

---
+++ trunk/BNUBot/src/bnubot/core/friend/FriendEntry.java
@@ -0,0 +1,21 @@
+package bnubot.core.friend;
+
+public class FriendEntry {
+	String account;
+	byte status;
+	byte location;
+	int product;
+	String locationName;
+	
+	public FriendEntry(String account, byte status, byte location, int product, String locationName) {
+		this.account = account;
+		this.status = status;
+		this.location = location;
+		this.product = product;
+		this.locationName = locationName;
+	}
+	
+	public String toString() {
+		return account + " (locationName=" + locationName + ")";
+	}
+}
