673c0e2518c9358965da732a27cac90ad113f0b5
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-06 20:26:59 +0000

    Refactored code; moved 'synchronized' keywork up to Connection class

--- trunk/BNUBot/src/bnubot/bot/EventHandler.java
+++ trunk/BNUBot/src/bnubot/bot/EventHandler.java
@@ -2,13 +2,18 @@ package bnubot.bot;
 
 import bnubot.core.BNetUser;
 import bnubot.core.Connection;
+import bnubot.core.clan.ClanMember;
+import bnubot.core.friend.FriendEntry;
 
 public interface EventHandler {
+	//Initialization
 	public void initialize(Connection c);
 	
+	//Connection
 	public void bnetConnected();
 	public void bnetDisconnected();
-	
+
+	//Channel events
 	public void joinedChannel(String channel);
 	public void channelUser(BNetUser user, int flags, int ping, String statstr);
 	public void channelJoin(BNetUser user, int flags, int ping, String statstr);
@@ -19,4 +24,12 @@ public interface EventHandler {
 	public void recieveError(String text);
 	public void whisperSent(BNetUser user, int flags, int ping, String text);
 	public void whisperRecieved(BNetUser user, int flags, int ping, String text);
+	
+	//Friends
+	public void friendsList(FriendEntry[] entries);
+	public void friendsUpdate(byte entry, byte location, byte status, int product, String locationName);
+	
+	//Clan
+	public void clanMemberList(ClanMember[] members);
+	public void clanMemberRankChange(byte oldRank, byte newRank, String user);
 }
--- trunk/BNUBot/src/bnubot/bot/console/ConsoleEventHandler.java
+++ trunk/BNUBot/src/bnubot/bot/console/ConsoleEventHandler.java
@@ -3,6 +3,8 @@ package bnubot.bot.console;
 import bnubot.bot.EventHandler;
 import bnubot.core.BNetUser;
 import bnubot.core.Connection;
+import bnubot.core.clan.ClanMember;
+import bnubot.core.friend.FriendEntry;
 
 public class ConsoleEventHandler implements EventHandler {
 	Connection c = null;
@@ -12,47 +14,52 @@ public class ConsoleEventHandler implements EventHandler {
 		new CLIThread(c).start();
 	}
 
-	public synchronized void joinedChannel(String channel) {
+	public void joinedChannel(String channel) {
 		System.out.println("Joining channel " + channel);
 	}
 
-	public synchronized void channelUser(BNetUser user, int flags, int ping, String statstr) {
+	public void channelUser(BNetUser user, int flags, int ping, String statstr) {
 		if(statstr.length() > 4)
 			statstr = statstr.substring(0, 4);
 		System.out.println(user + " (" + ping + "ms) " + statstr);
 	}
 	
-	public synchronized void channelJoin(BNetUser user, int flags, int ping, String statstr) {
+	public void channelJoin(BNetUser user, int flags, int ping, String statstr) {
 		System.out.println(user + " has joined the channel.");
 	}
-	public synchronized void channelLeave(BNetUser user, int flags, int ping, String statstr) {
+	public void channelLeave(BNetUser user, int flags, int ping, String statstr) {
 		System.out.println(user + " has left the channel.");
 	}
 
-	public synchronized void recieveChat(BNetUser user, int flags, int ping, String text) {
+	public void recieveChat(BNetUser user, int flags, int ping, String text) {
 		System.out.println("<" + user + "> " + text);
 	}
 
-	public synchronized void recieveEmote(BNetUser user, int flags, int ping, String text) {
+	public void recieveEmote(BNetUser user, int flags, int ping, String text) {
 		System.out.println("<" + user + " " + text + ">");
 	}
 
-	public synchronized void whisperRecieved(BNetUser user, int flags, int ping, String text) {
+	public void whisperRecieved(BNetUser user, int flags, int ping, String text) {
 		System.out.println("<From: " + user + "> " + text);
 	}
 
-	public synchronized void whisperSent(BNetUser user, int flags, int ping, String text) {
+	public void whisperSent(BNetUser user, int flags, int ping, String text) {
 		System.out.println("<To: " + user + "> " + text);
 	}
 
-	public synchronized void recieveInfo(String text) {
+	public void recieveInfo(String text) {
 		System.out.println(text);
 	}
 
-	public synchronized void recieveError(String text) {
+	public void recieveError(String text) {
 		System.err.println(text);
 	}
 
-	public synchronized void bnetConnected() {}
-	public synchronized void bnetDisconnected() {}
+	public void bnetConnected() {}
+	public void bnetDisconnected() {}
+
+	public void friendsList(FriendEntry[] entries) {}
+	public void friendsUpdate(byte entry, byte location, byte status, int product, String locationName) {}
+	public void clanMemberRankChange(byte oldRank, byte newRank, String user) {}
+	public void clanMemberList(ClanMember[] members) {}
 }
