875ec52e81c2e72ba0f8b76211d19fe791a7e041
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-11 13:25:14 +0000

    UnsupportedFeatureException

---
+++ BNUBot/src/bnubot/core/UnsupportedFeatureException.java
@@ -0,0 +1,10 @@
+package bnubot.core;
+
+@SuppressWarnings("serial")
+public class UnsupportedFeatureException extends Exception {
+
+	public UnsupportedFeatureException(String string) {
+		super(string);
+	}
+
+}
