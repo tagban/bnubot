3ed725b387fc64f7bf1aef3d6f93a1c28433092f
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-12 21:00:39 +0000

    *** empty log message ***

---
+++ BNUBot/src/bnubot/Version.java
@@ -0,0 +1,19 @@
+package bnubot;
+
+public final class Version {
+	public static final Integer VER_MAJOR = 2;
+	public static final Integer VER_MINOR = 0;
+	public static final Integer VER_REVISION = 0;
+	public static final Integer VER_ALPHA = null;
+	public static final Integer VER_BETA = 2;
+	
+	public static final String version() {
+		String out = VER_MAJOR.toString() + '.' + VER_MINOR + '.' + VER_REVISION;
+		if(VER_ALPHA != null)
+			out += " alpha " + VER_ALPHA;
+		if(VER_BETA != null)
+			out += " beta " + VER_BETA;
+		
+		return out;
+	}
+}
