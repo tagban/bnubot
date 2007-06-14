3876376b2affd338705abb3bab17ca3436b3c99e
camel <camel@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-06-14 04:08:13 +0000

    Beta 3

--- BNUBot/src/bnubot/Version.java
+++ BNUBot/src/bnubot/Version.java
@@ -1,11 +1,11 @@
 package bnubot;
 
 public final class Version {
-	public static final Integer VER_MAJOR = 3;
+	public static final Integer VER_MAJOR = 2;
 	public static final Integer VER_MINOR = 0;
 	public static final Integer VER_REVISION = 0;
 	public static final Integer VER_ALPHA = null;
-	public static final Integer VER_BETA = 2;
+	public static final Integer VER_BETA = 3;
 	
 	public static final String version() {
 		String out = VER_MAJOR.toString() + '.' + VER_MINOR + '.' + VER_REVISION;
