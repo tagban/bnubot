307db82e46f4210e2bb1cccd160eac40f2e87699
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-07-23 15:43:28 +0000

    Got rid of the INI file nonsense

---
+++ trunk/BNUBot/src/bnubot/util/Settings.java
@@ -0,0 +1,58 @@
+package bnubot.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Properties;
+
+import bnubot.Version;
+
+public class Settings {
+	private static File propsFile = null;
+	private static Properties props = null;
+	private static final String comments = "BNUBot " + Version.version();
+	
+	private static void init() {
+		if(props != null)
+			return;
+		
+		props = new Properties();
+		propsFile = new File("settings.xml");
+		
+		if(propsFile.exists()) try {
+			props.load(new FileInputStream(propsFile));
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static String getKey(String Header, String Setting) {
+		return Header + "_" + Setting;
+	}
+	
+	public static String read(String Header, String Setting, String Default) {
+		init();
+		String s = props.getProperty(getKey(Header, Setting));
+		if(s != null)
+			return s;
+		return Default;
+	}
+	
+	public static void write(String Header, String Setting, String Value) {
+		init();
+		
+		String key = getKey(Header, Setting);
+		if(Value == null)
+			Value = new String();
+		
+		props.setProperty(key, Value);
+		
+		try {
+			props.store(new FileOutputStream(propsFile), comments);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+}
