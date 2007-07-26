2c95a53b9eb5a2252196dafa83b52e85944b4b5e
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-07-26 16:15:16 +0000

    All other Version classes have inferior potassium

--- trunk/BNUBot/src/bnubot/Version.java
+++ trunk/BNUBot/src/bnubot/Version.java
@@ -5,6 +5,8 @@
 
 package bnubot;
 
+import java.io.*;
+
 public final class Version {
 	public static final Integer VER_MAJOR = 2;
 	public static final Integer VER_MINOR = 0;
@@ -12,13 +14,81 @@ public final class Version {
 	public static final Integer VER_RELEASE_CANDIDATE = null;
 	public static final Integer VER_ALPHA = null;
 	public static final Integer VER_BETA = 3;
+	private static Integer VER_SVN_REVISION = null;
 	private static String VER_STRING = null;
 	
-	public static final Long revision() {
-		String rev = "$Revision$";
-		rev = rev.substring(rev.indexOf(' ') + 1);
-		rev = rev.substring(0, rev.indexOf(' '));
-		return Long.parseLong(rev);
+	private static final Integer revision(File f) {
+		Integer r = null;
+		for(File sf : f.listFiles()) {
+			if(sf.isDirectory()) {
+				Integer r2 = revision(sf);
+				if((r2 != null) && ((r == null) || (r2 > r)))
+					r = r2;
+				continue;
+			}
+			
+			int ext = sf.getName().indexOf(".java");
+			if(ext != sf.getName().length() - 5)
+				continue;
+			
+			try {
+				FileReader fr = new FileReader(sf);
+				do {
+					int i = fr.read();
+					if(i == -1) { // <EOF>
+						System.err.println("Couldn't find Id: tag in " + sf.getPath());
+						break;
+					}
+					
+					// Search for "$Id: " 
+					if(i != '$')
+						continue;
+					if(fr.read() != 'I')
+						continue;
+					if(fr.read() != 'd')
+						continue;
+					if(fr.read() != ':')
+						continue;
+					if(fr.read() != ' ')
+						continue;
+
+					// Skip over the filename
+					while(fr.read() != ' ');
+					
+					// Read in the revision as a String
+					String rev = "";
+					do { 
+						int c = fr.read();
+						if(c < '0')
+							break;
+						if(c > '9')
+							break;
+						
+						rev += (char) c;
+					} while(true);
+					
+					if(rev.length() == 0)
+						continue;
+					
+					// Parse the long
+					int r2 = Integer.parseInt(rev);
+					if((r == null) || (r2 > r))
+						r = r2;
+					
+					break;
+				} while(true);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return r;
+	}
+	
+	public static final Integer revision() {
+		if(VER_SVN_REVISION == null)
+			VER_SVN_REVISION = revision(new File("."));
+		
+		return VER_SVN_REVISION;
 	}
 	
 	public static final String version() {
@@ -33,7 +103,8 @@ public final class Version {
 		else if(VER_RELEASE_CANDIDATE != null)
 			VER_STRING += " RC " + VER_RELEASE_CANDIDATE.toString();
 		
-		VER_STRING += " r" + revision().toString();
+		if(revision() != null)
+			VER_STRING += " (r" + VER_SVN_REVISION.toString() + ")";
 		
 		return VER_STRING;
 	}
