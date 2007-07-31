bf154ae3acec448112ba8085b8583a18946229d7
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-07-31 16:07:24 +0000

    Credit to Protege-2000, SVN Id: in BrowserLauncher, CurrentVersion wont re-generate SVN revision

--- BNUBot/src/bnubot/util/BrowserLauncher.java
+++ BNUBot/src/bnubot/util/BrowserLauncher.java
@@ -1,3 +1,8 @@
+/**
+ * This file is distributed under the Mozilla Public License Version 1.1
+ * $Id$
+ */
+
 /*
  * The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License");  you may not use this file except in compliance
