8961af364f82574b71c3d8fe83c56056b6ccffd7
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-08-21 20:15:53 +0000

    Removed unused code

--- BNUBot/src/net/bnubot/core/bncs/ProductIDs.java
+++ BNUBot/src/net/bnubot/core/bncs/ProductIDs.java
@@ -5,8 +5,6 @@
 
 package net.bnubot.core.bncs;
 
-import util.Constants;
-
 public abstract class ProductIDs {
 	public static final int	PRODUCT_CHAT	= 0x43484154;
 	public static final int	PRODUCT_STAR	= 0x53544152;
@@ -76,23 +74,4 @@ public abstract class ProductIDs {
 		PRODUCT_DSHR,
 		PRODUCT_SSHR
 	};
-	
-	public String getProdStr(int prod) {
-		switch(prod) {
-		case PRODUCT_CHAT:	return "CHAT";
-		case PRODUCT_STAR:	return Constants.prods[Constants.PRODUCT_STARCRAFT - 1];
-		case PRODUCT_SEXP:	return Constants.prods[Constants.PRODUCT_BROODWAR - 1];
-		case PRODUCT_W2BN:	return Constants.prods[Constants.PRODUCT_WAR2BNE - 1];
-		case PRODUCT_D2DV:	return Constants.prods[Constants.PRODUCT_DIABLO2 - 1];
-		case PRODUCT_D2XP:	return Constants.prods[Constants.PRODUCT_LORDOFDESTRUCTION - 1];
-		case PRODUCT_JSTR:	return Constants.prods[Constants.PRODUCT_JAPANSTARCRAFT - 1];
-		case PRODUCT_WAR3:	return Constants.prods[Constants.PRODUCT_WARCRAFT3 - 1];
-		case PRODUCT_W3XP:	return Constants.prods[Constants.PRODUCT_THEFROZENTHRONE - 1];
-		case PRODUCT_DRTL:	return Constants.prods[Constants.PRODUCT_DIABLO - 1];
-		case PRODUCT_DSHR:	return Constants.prods[Constants.PRODUCT_DIABLOSHAREWARE - 1];
-		case PRODUCT_SSHR:	return Constants.prods[Constants.PRODUCT_STARCRAFTSHAREWARE - 1];
-		default:
-			return "????";
-		}
-	}
 }
