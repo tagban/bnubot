a1388ea1290827e777ace962f8ebd257bd09053c
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-08-27 16:13:04 +0000

    formatTime for ms

--- trunk/BNUBot/src/net/bnubot/util/TimeFormatter.java
+++ trunk/BNUBot/src/net/bnubot/util/TimeFormatter.java
@@ -13,23 +13,26 @@ public class TimeFormatter {
 
 	public static String formatTime(long time) {
 		String text = "";
+		if(time < 60*1000)
+			text = Long.toString(time % 1000) + "ms";
 		time /= 1000;
-		if(time < 60*60)
-			text = Long.toString(time % 60) + "s";
-		time /= 60;
 		if(time > 0) {
-			if(time < 60*24)
-				text = Long.toString(time % 60) + "m " + text;
+			if(time < 60*60)
+				text = Long.toString(time % 60) + "s " + text;
 			time /= 60;
 			if(time > 0) {
-				if(time < 24*7)
-					text = Long.toString(time % 24) + "h " + text;
-				time /= 24;
-				if(time > 0)
-					text = Long.toString(time) + "d " + text;
+				if(time < 60*24)
+					text = Long.toString(time % 60) + "m " + text;
+				time /= 60;
+				if(time > 0) {
+					if(time < 24*7)
+						text = Long.toString(time % 24) + "h " + text;
+					time /= 24;
+					if(time > 0)
+						text = Long.toString(time) + "d " + text;
+				}
 			}
 		}
-		
 		return text.trim();
 	}
 	
