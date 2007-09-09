c3ee2133128ef1d06d24e484a3029a10f49a2795
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-09-09 05:46:02 +0000

    Dynamic driver loading

---
+++ trunk/BNUBot/src/net/bnubot/JARLoader.java
@@ -0,0 +1,46 @@
+package net.bnubot;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import net.bnubot.util.Out;
+
+public class JARLoader {
+	private static final FilenameFilter fnf = new FilenameFilter() {
+		public boolean accept(File dir, String name) {
+			return name.endsWith(".jar");
+		}};
+	private static URLClassLoader loader;
+	static {
+		String folder = "lib";
+		
+		File f = new File(folder);
+		if(!f.exists() || !f.isDirectory())
+			Out.fatalException(new FileNotFoundException(f.getName()));
+		
+		String[] files = f.list(fnf);
+		URL[] urls = new URL[files.length];
+		for(int i = 0; i < files.length; i++)
+			try {
+				Out.info(JARLoader.class, "Loading " + files[i]);
+				urls[i] = new URL("file:" + folder + "/" + files[i]);
+				urls[i].openStream();
+			} catch (MalformedURLException e) {
+				Out.exception(e);
+			} catch (IOException e) {
+				Out.exception(e);
+			}
+		
+		loader = new URLClassLoader(urls);
+	}
+	
+	public static Class<?> forName(String name) throws ClassNotFoundException {
+		Out.info(JARLoader.class, name);
+		return loader.loadClass(name);
+	}
+}
