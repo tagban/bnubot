497e05278827ed3cde20a885b7cf696498a71bcf
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-09-23 23:24:17 +0000

    Fixed regex for URLs beginning with newlines

--- BNUBot/src/net/bnubot/bot/gui/components/TextWindow.java
+++ BNUBot/src/net/bnubot/bot/gui/components/TextWindow.java
@@ -123,13 +123,13 @@ public class TextWindow extends JScrollPane {
 	private static Pattern pattern = null;
 	public String safeHtml(String in) {
 		if(pattern == null)
-			pattern = Pattern.compile("(.*?)\\b((([a-zA-Z]{3,6}://)|(www.)){1}([a-zA-Z0-9-.]+)([^-]\\.[a-zA-Z]{2,5}){1}((/\\S+){1}|\\s*?))(.*)");
+			pattern = Pattern.compile("((.|\n)*?)\\b((([a-zA-Z]{3,6}://)|(www.)){1}([a-zA-Z0-9-.]+)([^-]\\.[a-zA-Z]{2,5}){1}((/\\S+){1}|\\s*?))((.|\n)*)");
 		Matcher matcher = pattern.matcher(in); 
 		
 		if(matcher.matches())
 			return safeHtml(matcher.group(1))
-				+ "<a href=\"" + matcher.group(2) + "\">" + matcher.group(2) + "</a>"
-				+ safeHtml(matcher.group(10));
+				+ "<a href=\"" + matcher.group(3) + "\">" + matcher.group(3) + "</a>"
+				+ safeHtml(matcher.group(11));
 		return in
 			.replaceAll("&", "&amp;")
 			.replaceAll("<", "&lt;")
