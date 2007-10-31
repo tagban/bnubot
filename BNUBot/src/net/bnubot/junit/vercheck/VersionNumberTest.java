88d29e6569db347461f12c799685ac83dbc99684
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-10-31 17:17:49 +0000

    Set title for tasks window Added JUnit tests for Tasks Fixed VN JUnit test

--- BNUBot/src/net/bnubot/junit/vercheck/VersionNumberTest.java
+++ BNUBot/src/net/bnubot/junit/vercheck/VersionNumberTest.java
@@ -11,8 +11,8 @@ import junit.framework.TestCase;
 
 public class VersionNumberTest extends TestCase {
 	public void testVersionCompare() {
-		VersionNumber dev_1_0_0_10 = new VersionNumber(ReleaseType.Development, 1, 0, 0, null, 10, null);
-		VersionNumber stable_1_0_0_10 = new VersionNumber(ReleaseType.Stable, 1, 0, 0, null, 10, null);
-		assertEquals(0, dev_1_0_0_10.compareTo(stable_1_0_0_10));
+		VersionNumber dev_1_0_0_10 = new VersionNumber(ReleaseType.Development, 1, 0, 0, 10);
+		VersionNumber stable_1_0_0_10 = new VersionNumber(ReleaseType.Stable, 1, 0, 0, 10);
+		assertEquals(-1, dev_1_0_0_10.compareTo(stable_1_0_0_10));
 	}
 }
