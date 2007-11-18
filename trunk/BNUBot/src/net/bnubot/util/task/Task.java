c6e25520c5356945f7b148ad164da2797c68e574
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-11-18 21:26:17 +0000

    Updated to avoid crash in deamon mode

---
+++ trunk/BNUBot/src/net/bnubot/util/task/Task.java
@@ -0,0 +1,12 @@
+/**
+ * This file is distributed under the GPL 
+ * $Id$
+ */
+
+package net.bnubot.util.task;
+
+public class Task {
+	public void updateProgress(String currentStep) {}
+	public void advanceProgress() {}
+	public void complete() {}
+}
