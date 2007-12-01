b8b5e03b51d7fa508a6a784942fa3ca072f960aa
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2007-12-01 21:31:22 +0000

    Resolved the unchecked issue

--- BNUBot/src/net/bnubot/util/SortedProperties.java
+++ BNUBot/src/net/bnubot/util/SortedProperties.java
@@ -6,20 +6,23 @@
 package net.bnubot.util;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.Properties;
 import java.util.Vector;
 
 public class SortedProperties extends Properties {
 	private static final long serialVersionUID = 213081373529965920L;
+	
+	private static final Comparator<Object> comparator = new Comparator<Object>() {
+		public int compare(Object o1, Object o2) {
+			return ((String)o1).compareTo((String)o2);
+		}
+	};
 
-	@SuppressWarnings("unchecked")
-	public synchronized Enumeration keys() {
-		Enumeration keysEnum = super.keys();
-		Vector keyList = new Vector();
-		while(keysEnum.hasMoreElements())
-			keyList.add(keysEnum.nextElement());
-		Collections.sort(keyList);
+	public synchronized Enumeration<Object> keys() {
+		Vector<Object> keyList = new Vector<Object>(super.keySet());
+		Collections.sort(keyList, comparator);
 		return keyList.elements();
 	}
 }
