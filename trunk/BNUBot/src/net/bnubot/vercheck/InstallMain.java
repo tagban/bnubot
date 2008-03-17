f7b769ce0f92b5ce62a19c55e073ff0281b9b7a5
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2008-03-17 17:49:54 +0000

    OS-dependant version check

--- trunk/BNUBot/src/net/bnubot/vercheck/InstallMain.java
+++ trunk/BNUBot/src/net/bnubot/vercheck/InstallMain.java
@@ -5,27 +5,16 @@
 
 package net.bnubot.vercheck;
 
-import java.io.File;
-import java.net.URL;
-
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 import net.bnubot.util.OperatingSystem;
 import net.bnubot.util.Out;
-import net.bnubot.util.URLDownloader;
 
 /**
  * @author scotta
  */
 public class InstallMain {
-	/**
-	 * Download helper for the installer
-	 */
-	private static void download(String url, String file) throws Exception {
-		URLDownloader.downloadURL(new URL(url + file), new File(file), null, false);
-	}
-	
 	public static void main(String[] args) throws Exception {
 		String downloadFolder = null;
 		try {
@@ -38,7 +27,7 @@ public class InstallMain {
 		
 		Out.setDebug(true);
 		
-		String command = "java -jar BNUBot.jar";
+		String command = "java -jar \"" + downloadFolder + "/BNUBot.jar\"";
 		String jarFileName = "BNUBot.jar";
 		
 		switch(OperatingSystem.userOS) {
@@ -46,30 +35,22 @@ public class InstallMain {
 			// We're on OSX, so let's try to create an application
 			String appdir = "BNUBot.app";
 			
-			Out.info(InstallMain.class, "Creating " + appdir);
-			new File(appdir).mkdir();
-			new File(appdir + "/Contents").mkdir();
-			new File(appdir + "/Contents/MacOS").mkdir();
-			new File(appdir + "/Contents/Resources").mkdir();
-			new File(appdir + "/Contents/Resources/Java").mkdir();
-			
-			String url = "http://bnubot.googlecode.com/svn/trunk/BNUBot/Dist/OSX/";
-			download(url, appdir + "/Contents/Info.plist");
-			download(url, appdir + "/Contents/PkgInfo");
-			download(url, appdir + "/Contents/MacOS/JavaApplicationStub");
-			download(url, appdir + "/Contents/Resources/Icon.icns");
-
-			jarFileName = appdir + "/Contents/Resources/Java/" + jarFileName;
+			jarFileName = "Contents/Resources/Java/" + jarFileName;
 			if(downloadFolder == null)
 				downloadFolder = appdir;
 			else
 				downloadFolder += "/" + appdir;
-			command = appdir + "/Contents/MacOS/JavaApplicationStub";
+			command = downloadFolder + "/Contents/MacOS/JavaApplicationStub";
+			break;
+			
+		case WINDOWS:
+			command = downloadFolder + "/BNUBot.exe";
+			break;
 		}
 		
 		if(!VersionCheck.checkVersion(true, ReleaseType.Stable, jarFileName, downloadFolder)) {
 			try {
-				JOptionPane.showMessageDialog(null, "Update failed!", "Error", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Install failed!", "Error", JOptionPane.ERROR_MESSAGE);
 			} catch(Exception e) {}
 			
 			System.exit(1);
@@ -79,7 +60,9 @@ public class InstallMain {
 		
 		// If launching in OSX, chmod the stub
 		if(command.endsWith("/JavaApplicationStub")) {
-			int ret = rt.exec("chmod 755 " + command).waitFor();
+			final String cmd_chmod = "chmod 755 \"" + command + "\"";
+			Out.info(InstallMain.class, "Fixing JavaApplicationStub: " + cmd_chmod);
+			int ret = rt.exec(cmd_chmod).waitFor();
 			if(ret != 0)
 				throw new IllegalStateException(Integer.toString(ret));
 		}
