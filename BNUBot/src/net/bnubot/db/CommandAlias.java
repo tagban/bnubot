109c12c3b599daf6bf5c5dec126402c3ee807e21
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2008-04-03 06:26:45 +0000

    Integer -> int

--- BNUBot/src/net/bnubot/db/CommandAlias.java
+++ BNUBot/src/net/bnubot/db/CommandAlias.java
@@ -8,5 +8,6 @@ package net.bnubot.db;
 import net.bnubot.db.auto._CommandAlias;
 
 public class CommandAlias extends _CommandAlias {
+	private static final long serialVersionUID = -3633743075279729925L;
 
 }
--- BNUBot/src/net/bnubot/db/DbVersion.java
+++ BNUBot/src/net/bnubot/db/DbVersion.java
@@ -8,5 +8,6 @@ package net.bnubot.db;
 import net.bnubot.db.auto._DbVersion;
 
 public class DbVersion extends _DbVersion {
+	private static final long serialVersionUID = 8125213225914051347L;
 
 }
