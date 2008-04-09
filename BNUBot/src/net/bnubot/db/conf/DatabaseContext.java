d836dfa817d58648724fe49162525ade5630587b
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2008-04-09 23:04:10 +0000

    2.0.5

--- BNUBot/src/net/bnubot/db/conf/DatabaseContext.java
+++ BNUBot/src/net/bnubot/db/conf/DatabaseContext.java
@@ -6,12 +6,23 @@
 package net.bnubot.db.conf;
 
 
+import java.util.List;
+
+import net.bnubot.db.Account;
+import net.bnubot.db.BNLogin;
+import net.bnubot.db.Mail;
+import net.bnubot.db.Rank;
+import net.bnubot.util.Out;
+
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.access.DataContext;
 import org.apache.cayenne.conf.Configuration;
+import org.apache.cayenne.query.SelectQuery;
 
 public class DatabaseContext {
 	private static ThreadLocal<ObjectContext> contexts = new ThreadLocal<ObjectContext>();
+	private static long lastFlush = 0;
+	
 	static {
 		try {
 			Configuration.initializeSharedConfiguration();
@@ -29,6 +40,78 @@ public class DatabaseContext {
 			oc = DataContext.createDataContext();
 			contexts.set(oc);
 		}
+		
+		// Every 20 minutes...
+		long now = System.currentTimeMillis();
+		if(now - lastFlush > 20 * 60 * 1000) {
+			lastFlush = now;
+			flush(oc);
+		}
 		return oc;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void flush(ObjectContext context) {
+		for(BNLogin login : (List<BNLogin>)context.performQuery(new SelectQuery(BNLogin.class))) {
+			long age = login.getLastSeen().getTime();
+			age = System.currentTimeMillis() - age;
+			age /= 86400000l; // convert to days
+			
+			long maxAge = 90;
+			Account account = login.getAccount();
+			if(account != null) {
+				Rank rank = account.getRank();
+				if(rank != null)
+					maxAge = rank.getExpireDays();
+			}
+			
+			if(age <= maxAge)
+				continue;
+			
+			if(account != null)
+				try {
+					Mail.send(account, account, "Your login [ " + login.getLogin() + " ] has been removed due to inactivity (" + age + " days)");
+				} catch (Exception e) {
+					Out.exception(e);
+				}
+				
+			Out.error(DatabaseContext.class, "Removing " + login.getLogin() + " due to inactivity (" + age + " days)");
+			context.deleteObject(login);
+		}
+		
+		try {
+			context.commitChanges();
+		} catch(Exception e) {
+			context.rollbackChanges();
+			Out.exception(e);
+		}
+		
+		for(Account account : (List<Account>)context.performQuery(new SelectQuery(Account.class))) {
+			if(account.getBnLogins().size() != 0)
+				continue;
+			if(account.getRecruits().size() != 0) {
+				// Give them 0 access
+				account.setRank(Rank.get(0));
+				continue;
+			}
+			
+			Account recruiter = account.getRecruiter();
+			if(recruiter != null)
+				try {
+					Mail.send(recruiter, recruiter, "Your recruit [ " + account.getName() + " ] has been removed due to inactivity");
+				} catch (Exception e) {
+					Out.exception(e);
+				}
+			
+			Out.error(DatabaseContext.class, "Removing " + account.getName() + " which has no active BNLogins");
+			context.deleteObject(account);
+		}
+		
+		try {
+			context.commitChanges();
+		} catch(Exception e) {
+			context.rollbackChanges();
+			Out.exception(e);
+		}
 	}
 }
