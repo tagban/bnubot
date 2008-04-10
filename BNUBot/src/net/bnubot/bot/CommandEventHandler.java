903fdddf2c195ab73db58a459b30e17aab1bdbb6
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2008-04-10 18:29:44 +0000

    'search' and 'searchrank' commands

--- BNUBot/src/net/bnubot/bot/CommandEventHandler.java
+++ BNUBot/src/net/bnubot/bot/CommandEventHandler.java
@@ -752,6 +752,74 @@ public class CommandEventHandler implements EventHandler {
 			throws Exception {
 				source.sendChat(param, false);
 			}});
+		Profile.registerCommand("search", new CommandRunnable() {
+			public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
+			throws Exception {
+				if((params == null) || (params.length != 1)) {
+					user.sendChat("Use: %trigger%search <pattern>", whisperBack);
+					return;
+				}
+				
+				String out = "Users found: ";
+				int num = 0;
+				for(BNLogin login : BNLogin.search(params[0])) {
+					if(num >= 10) {
+						out += ", <more>";
+						break;
+					}
+					
+					if(num++ > 0)
+						out += ", ";
+					out += login.getLogin();
+				}
+				if(num == 0)
+					out = "No users found!";
+				user.sendChat(out, whisperBack);
+			}});
+		Profile.registerCommand("searchrank", new CommandRunnable() {
+			public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
+			throws Exception {
+				try {
+					if((params == null) || (params.length < 1) || (params.length > 2))
+						throw new InvalidUseException();
+				
+					int access;
+					try {
+						access = Integer.parseInt(params[0]);
+					} catch(NumberFormatException e) {
+						throw new InvalidUseException();
+					}
+					
+					int accessUpper = access;
+					if(params.length > 1)
+						try {
+							accessUpper = Integer.parseInt(params[1]);
+						} catch(NumberFormatException e) {
+							throw new InvalidUseException();
+						}
+					
+					String out = "Accounts found: ";
+					boolean first = true;
+					for(int i = access; i <= accessUpper; i++) {
+						Rank rank = Rank.get(i);
+						if(rank == null)
+							continue;
+						for(Account account : rank.getAccountArray()) {
+							if(!first)
+								out += ", "; 
+							first = false;
+							out += account.getName();
+							out += " {" + i + "}";
+						}
+					}
+					if(first)
+						out = "No users found!";
+					
+					user.sendChat(out, whisperBack);
+				} catch(InvalidUseException e) {
+					user.sendChat("Use: %trigger%searchrank <rank_lowest> [rank_highest]", whisperBack);
+				}
+			}});
 		Profile.registerCommand("seen", new CommandRunnable() {
 			public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
 			throws Exception {
