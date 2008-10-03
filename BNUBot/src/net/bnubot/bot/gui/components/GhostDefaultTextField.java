750d7451257344f4189ec529db6a5a10eb58147f
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2008-10-03 18:01:52 +0000

    ghosted defaults

---
+++ BNUBot/src/net/bnubot/bot/gui/components/GhostDefaultTextField.java
@@ -0,0 +1,61 @@
+/**
+ * This file is distributed under the GPL
+ * $Id$
+ */
+package net.bnubot.bot.gui.components;
+
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+
+/**
+ * @author scotta
+ */
+public class GhostDefaultTextField extends ConfigTextField implements MouseListener, FocusListener {
+	private static final long serialVersionUID = 5908386893553368742L;
+
+	private String defaultText;
+
+	/**
+	 * @param defaultText
+	 */
+	public GhostDefaultTextField(String defaultText) {
+		super(defaultText);
+		this.defaultText = defaultText;
+		addMouseListener(this);
+		addFocusListener(this);
+		setEnabled(false);
+	}
+
+	public void reset() {
+		setEnabled(false);
+		setText(defaultText);
+	}
+
+	public boolean isGhosted() {
+		focusLost(null);
+		return !isEnabled();
+	}
+
+	public void focusGained(FocusEvent e) {}
+	public void focusLost(FocusEvent e) {
+		if((getText() == null) || (getText().length() == 0))
+			reset();
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		if(isEnabled())
+			return;
+		setEnabled(true);
+		setText(null);
+		requestFocus();
+	}
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {}
+	public void mouseReleased(MouseEvent e) {}
+
+
+}
