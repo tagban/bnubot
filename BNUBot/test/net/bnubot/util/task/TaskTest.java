9f18174fd94f56b19e8ac3f55dcaf2a9c69bd687
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2008-10-21 20:07:42 +0000

    Task test

--- BNUBot/test/net/bnubot/util/task/TaskTest.java
+++ BNUBot/test/net/bnubot/util/task/TaskTest.java
@@ -6,6 +6,8 @@
 package net.bnubot.util.task;
 
 import junit.framework.TestCase;
+import net.bnubot.settings.GlobalSettings;
+import net.bnubot.settings.GlobalSettings.TrayIconMode;
 
 /**
  * @author scotta
@@ -13,6 +15,7 @@ import junit.framework.TestCase;
 public class TaskTest extends TestCase {
 
 	public void testTasks() throws Exception {
+		GlobalSettings.trayIconMode = TrayIconMode.DISABLED;
 		Task ind = TaskManager.createTask("indeterminate task");
 		Task det = TaskManager.createTask("determinate task", 10, "steps");
 		for(int i = 0; i < 10; i++) {
