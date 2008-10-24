5ac1f577336b720e9fa093752deaa0bbf0ed269a
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2008-10-24 18:17:08 +0000

    isOneOfMyUsers()

--- trunk/BNUBot/src/net/bnubot/bot/gui/GuiEventHandler.java
+++ trunk/BNUBot/src/net/bnubot/bot/gui/GuiEventHandler.java
@@ -614,7 +614,7 @@ public class GuiEventHandler extends EventHandler {
 
 	@Override
 	public void recieveChat(Connection source, BNetUser user, String text) {
-		mainTextArea.userChat(source.getDisplayType(), user, text, user.equals(source.getMyUser()));
+		mainTextArea.userChat(source.getDisplayType(), user, text, profile.isOneOfMyUsers(user));
 		if(GlobalSettings.trayDisplayChatEmote)
 			notifySystemTray(
 					Growl.CHANNEL_USER_CHAT,
