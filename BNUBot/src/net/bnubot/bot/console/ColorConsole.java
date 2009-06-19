cde3d0770c980c35658d296770ece5cfd881d7f1
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2009-06-19 00:53:51 +0000

    Color console

---
+++ BNUBot/src/net/bnubot/bot/console/ColorConsole.java
@@ -0,0 +1,68 @@
+package net.bnubot.bot.console;
+
+import java.io.PrintStream;
+
+public class ColorConsole {
+	public enum ColorConstant {
+		FG_BLACK(30),
+		FG_RED(31),
+		FG_GREEN(32),
+		FG_YELLOW(33),
+		FG_BLUE(34),
+		FG_MAGENTA(35),
+		FG_CYAN(36),
+		FG_WHITE(37),
+
+		BG_BLACK(40),
+		BG_RED(41),
+		BG_GREEN(42),
+		BG_YELLOW(43),
+		BG_BLUE(44),
+		BG_MAGENTA(45),
+		BG_CYAN(46),
+		BG_WHITE(47);
+
+		final int code;
+		ColorConstant(int code) {
+			this.code = code;
+		}
+	}
+
+	private PrintStream out;
+
+	public ColorConsole(PrintStream out) {
+		this.out = out;
+	}
+
+	public ColorConsole begin() {
+		color();
+		out.print("[00:00:00] ");
+		return this;
+	}
+
+	public void end() {
+		out.println();
+	}
+
+	public ColorConsole color(ColorConstant... color) {
+		out.print("\033[");
+		if(color.length == 0) {
+			// Reset color to normal
+			out.print("00");
+		} else {
+			out.print(color[0].code);
+		}
+
+		for(ColorConstant c : color) {
+			out.print(';');
+			out.print(c.code);
+		}
+		out.print("m");
+		return this;
+	}
+
+	public ColorConsole string(String string) {
+		out.print(string);
+		return this;
+	}
+}
--- BNUBot/src/net/bnubot/bot/console/ConsoleEventHandler.java
+++ BNUBot/src/net/bnubot/bot/console/ConsoleEventHandler.java
@@ -7,6 +7,7 @@ package net.bnubot.bot.console;
 
 import java.util.HashMap;
 
+import net.bnubot.bot.console.ColorConsole.ColorConstant;
 import net.bnubot.core.Connection;
 import net.bnubot.core.EventHandler;
 import net.bnubot.core.Profile;
@@ -18,6 +19,8 @@ import net.bnubot.util.BNetUser;
  */
 public class ConsoleEventHandler extends EventHandler {
 	private static final HashMap<Connection, CLIThread> threads = new HashMap<Connection, CLIThread>();
+
+	private static ColorConsole con = new ColorConsole(System.out);
 
 	public ConsoleEventHandler(Profile profile) {
 		super(profile);
@@ -39,73 +42,140 @@ public class ConsoleEventHandler extends EventHandler {
 
 	@Override
 	public void joinedChannel(Connection source, String channel) {
-		System.out.println("Joining channel " + channel);
+		con.begin()
+		.color(ColorConstant.FG_GREEN)
+		.string("Joining channel ")
+		.string(channel)
+		.end();
 	}
 
 	@Override
 	public void channelUser(Connection source, BNetUser user) {
 		if(GlobalSettings.displayChannelUsers)
-			System.out.println(user.toStringEx() + user.getStatString().toString());
+			con.begin()
+			.color(ColorConstant.FG_GREEN)
+			.string(user.toStringEx())
+			.string(user.getStatString().toString())
+			.end();
 	}
 
 	@Override
 	public void channelJoin(Connection source, BNetUser user) {
 		if(GlobalSettings.getDisplayJoinParts())
-			System.out.println(user.toStringEx() + " has joined the channel" + user.getStatString().toString() + ".");
+			con.begin()
+			.color(ColorConstant.FG_GREEN)
+			.string(user.toStringEx())
+			.string(" has joined the channel")
+			.string(user.getStatString().toString())
+			.string(".")
+			.end();
 	}
 	@Override
 	public void channelLeave(Connection source, BNetUser user) {
 		if(GlobalSettings.getDisplayJoinParts())
-			System.out.println(user.toStringEx() + " has left the channel.");
+			con.begin()
+			.color(ColorConstant.FG_GREEN)
+			.string(user.toStringEx())
+			.string(" has left the channel.")
+			.end();
 	}
 
 	@Override
 	public void recieveChat(Connection source, BNetUser user, String text) {
-		System.out.println("<" + user.toString() + "> " + text);
+		con.begin()
+		.string("<")
+		.string(user.toString())
+		.string("> ")
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void recieveBroadcast(Connection source, String username, int flags, String text) {
-		System.out.println("<" + username + "> " + text);
+		con.begin()
+		.string("<")
+		.string(username)
+		.string("> ")
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void recieveEmote(Connection source, BNetUser user, String text) {
-		System.out.println("<" + user.toString() + " " + text + ">");
+		con.begin()
+		.string("<")
+		.string(user.toString())
+		.string(" ")
+		.string(text)
+		.string(">")
+		.end();
 	}
 
 	@Override
 	public void whisperRecieved(Connection source, BNetUser user, String text) {
-		System.out.println("<From: " + user.toString() + "> " + text);
+		con.begin()
+		.string("<From: ")
+		.string(user.toString())
+		.string("> ")
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void whisperSent(Connection source, BNetUser user, String text) {
-		System.out.println("<To: " + user.toString() + "> " + text);
+		con.begin()
+		.string("<To: ")
+		.string(user.toString())
+		.string("> ")
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void recieveDebug(Connection source, String text) {
-		System.out.println("(DEBUG) " + text);
+		con.begin()
+		.color(ColorConstant.FG_YELLOW)
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void recieveInfo(Connection source, String text) {
-		System.out.println("(INFO) " + text);
+		con.begin()
+		.color(ColorConstant.FG_BLUE)
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void recieveError(Connection source, String text) {
-		System.out.println("(ERROR) " + text);
+		con.begin()
+		.color(ColorConstant.FG_RED)
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void recieveServerInfo(Connection source, String text) {
-		System.out.println("(INFO) " + text);
+		con.begin()
+		.color(ColorConstant.FG_MAGENTA)
+		.string("(")
+		.string(source.getServerType())
+		.string(") ")
+		.color(ColorConstant.FG_BLUE)
+		.string(text)
+		.end();
 	}
 
 	@Override
 	public void recieveServerError(Connection source, String text) {
-		System.out.println("(ERROR) " + text);
+		con.begin()
+		.color(ColorConstant.FG_MAGENTA)
+		.string("(")
+		.string(source.getServerType())
+		.string(") ")
+		.color(ColorConstant.FG_RED)
+		.string(text)
+		.end();
 	}
 }
