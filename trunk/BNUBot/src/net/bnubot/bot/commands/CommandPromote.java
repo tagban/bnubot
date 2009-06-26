a5e2f895d9c1240cf4198444ec9b1847add76af7
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2009-06-26 19:30:43 +0000

    Closes Issue 81 Promote/demote commands

---
+++ trunk/BNUBot/src/net/bnubot/bot/commands/CommandPromote.java
@@ -0,0 +1,34 @@
+/**
+ * This file is distributed under the GPL
+ * $Id$
+ */
+package net.bnubot.bot.commands;
+
+import net.bnubot.bot.CommandEventHandler;
+import net.bnubot.core.Connection;
+import net.bnubot.core.commands.CommandRunnable;
+import net.bnubot.core.commands.InvalidUseException;
+import net.bnubot.db.Account;
+import net.bnubot.util.BNetUser;
+
+/**
+ * @author scotta
+ */
+public final class CommandPromote implements CommandRunnable {
+	public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
+	throws Exception {
+		try {
+			if(params == null)
+				throw new InvalidUseException();
+			if(params.length != 1)
+				throw new InvalidUseException();
+
+			Account subjectAccount = CommandEventHandler.findOrCreateAccount(source, user, commanderAccount, params[0], whisperBack);
+
+			int targetAccess = subjectAccount.getAccess() + 1;
+			CommandEventHandler.setAccountAccess(user, commanderAccount, subjectAccount, targetAccess, superUser, whisperBack);
+		} catch(InvalidUseException e) {
+			user.sendChat("Use: %trigger%promote <account>", whisperBack);
+		}
+	}
+}
