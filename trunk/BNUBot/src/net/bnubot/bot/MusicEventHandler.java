04d5b172a7a22dc0ecf9b5104774f694aa3b4d90
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2009-07-17 15:24:02 +0000

    MusicEventHandler adds play pause and music commands

---
+++ trunk/BNUBot/src/net/bnubot/bot/MusicEventHandler.java
@@ -0,0 +1,55 @@
+/**
+ * This file is distributed under the GPL
+ * $Id$
+ */
+package net.bnubot.bot;
+
+import net.bnubot.core.Connection;
+import net.bnubot.core.EventHandler;
+import net.bnubot.core.Profile;
+import net.bnubot.core.commands.CommandRunnable;
+import net.bnubot.db.Account;
+import net.bnubot.db.conf.DatabaseContext;
+import net.bnubot.util.BNetUser;
+import net.bnubot.util.UnloggedException;
+import net.bnubot.util.music.MusicController;
+import net.bnubot.util.music.MusicControllerFactory;
+
+/**
+ * @author scotta
+ */
+public class MusicEventHandler extends EventHandler {
+
+	public MusicEventHandler(Profile profile) {
+		super(profile);
+		if(DatabaseContext.getContext() == null)
+			throw new UnloggedException("Can not enable commands without a database!");
+		initializeCommands();
+	}
+
+	private static boolean commandsInitialized = false;
+	private static void initializeCommands() {
+		if(commandsInitialized)
+			return;
+		commandsInitialized = true;
+
+		Profile.registerCommand("music", new CommandRunnable() {
+			public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
+			throws Exception {
+				MusicController mc = MusicControllerFactory.getMusicController();
+				user.sendChat(mc.getCurrentlyPlaying(), whisperBack);
+			}});
+		Profile.registerCommand("pause", new CommandRunnable() {
+			public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
+			throws Exception {
+				MusicController mc = MusicControllerFactory.getMusicController();
+				mc.pause();
+			}});
+		Profile.registerCommand("play", new CommandRunnable() {
+			public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
+			throws Exception {
+				MusicController mc = MusicControllerFactory.getMusicController();
+				mc.play();
+			}});
+	}
+}
