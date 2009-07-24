10cd6d28e040961c23ff087c97fcfb94ed4c358e
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2009-07-24 16:01:28 +0000

    !mail delete <number>

--- trunk/BNUBot/src/net/bnubot/bot/commands/CommandMail.java
+++ trunk/BNUBot/src/net/bnubot/bot/commands/CommandMail.java
@@ -15,11 +15,11 @@ import net.bnubot.core.commands.CommandRunnable;
 import net.bnubot.core.commands.InvalidUseException;
 import net.bnubot.db.Account;
 import net.bnubot.db.Mail;
-import net.bnubot.logging.Out;
 import net.bnubot.util.BNetUser;
 import net.bnubot.util.TimeFormatter;
 
 import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.query.RefreshQuery;
 
 /**
  * @author scotta
@@ -27,10 +27,8 @@ import org.apache.cayenne.ObjectContext;
 public final class CommandMail implements CommandRunnable {
 	public void run(Connection source, BNetUser user, String param, String[] params, boolean whisperBack, Account commanderAccount, boolean superUser)
 	throws Exception {
-		if(commanderAccount == null) {
-			user.sendChat("You must have an account to use mail.", whisperBack);
-			return;
-		}
+		if(commanderAccount == null)
+			throw new CommandFailedWithDetailsException("You must have an account to use mail.");
 
 		try {
 			if((params == null) || (params.length < 1))
@@ -66,14 +64,9 @@ public final class CommandMail implements CommandRunnable {
 					}
 				}
 
-				List<Mail> rsMail = commanderAccount.getRecievedMail();
-				// Sort the mail by sent date
-				Collections.sort(rsMail, new Comparator<Mail>() {
-					public int compare(Mail arg0, Mail arg1) {
-						return arg0.getSent().compareTo(arg1.getSent());
-					}});
+				List<Mail> rsMail = getMail(commanderAccount);
 				if(all) {
-					for(Mail m : rsMail)
+					for(Mail m : getMail(commanderAccount))
 						sendMail(user, whisperBack, ++id, rsMail.size(), m);
 				} else if(id == 0) {
 					for(Mail m : rsMail) {
@@ -88,16 +81,47 @@ public final class CommandMail implements CommandRunnable {
 					String message = "You have no unread mail!";
 					if(rsMail.size() > 0)
 						message += " To read your " + rsMail.size() + " messages, type [ %trigger%mail read <number> ]";
-					user.sendChat(message, whisperBack);
+					throw new CommandFailedWithDetailsException(message);
 				} else {
 					if((rsMail.size() >= id) && (id >= 1))
 						sendMail(user, whisperBack, id, rsMail.size(), rsMail.get(id-1));
 					else
-						user.sendChat("You only have " + rsMail.size() + " messages!", whisperBack);
+						throw new CommandFailedWithDetailsException("You only have " + rsMail.size() + " messages!");
 				}
 				return;
+			} else if(params[0].equals("delete")) {
+				//delete <number>
+				if(params.length != 2)
+					throw new InvalidUseException();
+
+				int id;
+				try {
+					id = Integer.parseInt(params[1]);
+				} catch(Exception e) {
+					throw new InvalidUseException();
+				}
+
+				List<Mail> rsMail = getMail(commanderAccount);
+
+				if((rsMail.size() >= id) && (id >= 1)) {
+					Mail m = rsMail.get(id-1);
+					String response = "Deleted " + constructMessage(id, rsMail.size(), m);
+
+					try {
+						ObjectContext context = commanderAccount.getObjectContext();
+						context.deleteObject(m);
+						commanderAccount.updateRow();
+						user.sendChat(response, whisperBack);
+
+						context.performQuery(new RefreshQuery(commanderAccount));
+					} catch(Exception e) {
+						throw new CommandFailedWithDetailsException("Failed to delete mail", e);
+					}
+				} else {
+					throw new CommandFailedWithDetailsException("You only have " + rsMail.size() + " messages!");
+				}
+
 			} else if(params[0].equals("empty")
-					||params[0].equals("delete")
 					||params[0].equals("clear")) {
 				//empty
 				if(params.length != 1)
@@ -118,11 +142,21 @@ public final class CommandMail implements CommandRunnable {
 			} else
 				throw new InvalidUseException();
 		} catch(InvalidUseException e) {
-			user.sendChat("Use: %trigger%mail (read [number] | empty | send <account> <message>)", whisperBack);
+			user.sendChat("Use: %trigger%mail (read [number] | delete <number> | empty | send <account> <message>)", whisperBack);
 		}
 	}
 
-	private void sendMail(BNetUser user, boolean whisperBack, int id, int size, Mail m) {
+	private List<Mail> getMail(Account account) {
+		List<Mail> rsMail = account.getRecievedMail();
+		// Sort the mail by sent date
+		Collections.sort(rsMail, new Comparator<Mail>() {
+			public int compare(Mail arg0, Mail arg1) {
+				return arg0.getSent().compareTo(arg1.getSent());
+			}});
+		return rsMail;
+	}
+
+	private String constructMessage(int id, int size, Mail m) {
 		StringBuilder message = new StringBuilder("#");
 		message.append(id);
 		message.append(" of ");
@@ -135,15 +169,18 @@ public final class CommandMail implements CommandRunnable {
 		message.append(TimeFormatter.formatTime(System.currentTimeMillis() - m.getSent().getTime()));
 		message.append(" ago]: ");
 		message.append(m.getMessage());
+		return message.toString();
+	}
 
-		user.sendChat(message.toString(), true);
+	private void sendMail(BNetUser user, boolean whisperBack, int id, int size, Mail m)
+	throws CommandFailedWithDetailsException {
+		user.sendChat(constructMessage(id, size, m), true);
 
 		m.setIsread(true);
 		try {
 			m.updateRow();
 		} catch(Exception e) {
-			Out.exception(e);
-			user.sendChat("Failed to set mail read", whisperBack);
+			throw new CommandFailedWithDetailsException("Failed to set mail read", e);
 		}
 	}
 }
