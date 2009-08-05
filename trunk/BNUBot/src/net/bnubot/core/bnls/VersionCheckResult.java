608834d8a010a59ad48c4fb0e0b35c356fa49951
scotta <scotta@a5b3554b-4b34-0410-b29a-9df7c87e797e>
2009-08-05 21:41:59 +0000

    BNLS code refactoring

---
+++ trunk/BNUBot/src/net/bnubot/core/bnls/VersionCheckResult.java
@@ -0,0 +1,12 @@
+/**
+ * This file is distributed under the GPL
+ * $Id$
+ */
+
+package net.bnubot.core.bnls;
+
+public class VersionCheckResult {
+	public int exeHash = 0;
+	public int exeVersion = 0;
+	public byte[] exeInfo = null;
+}
